57b770a3393982ad5038c2bf1df35c93a64d9223
elagarrigue <elagarriguelazarte@doximity.com>
2025-02-18 14:00:46 -0300

    Add dummy detail

--- composeApp/src/commonMain/kotlin/edu/eati25/kmp/movies/App.kt
+++ composeApp/src/commonMain/kotlin/edu/eati25/kmp/movies/App.kt
@@ -1,11 +1,13 @@
 package edu.eati25.kmp.movies
 
 import androidx.compose.runtime.Composable
+import edu.eati25.kmp.movies.ui.screens.DetailScreen
 import edu.eati25.kmp.movies.ui.screens.HomeScreen
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
-    HomeScreen()
+    //HomeScreen()
+    DetailScreen()
 }
---
+++ composeApp/src/commonMain/kotlin/edu/eati25/kmp/movies/ui/screens/DetailScreen.kt
@@ -0,0 +1,71 @@
+package edu.eati25.kmp.movies.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import edu.eati25.kmp.movies.movies
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailScreen() {
+    val movie = movies[0]
+    MaterialTheme {
+        Surface {
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        title = { Text(text = movie.title) },
+                        navigationIcon = {
+                            IconButton(
+                                onClick = { /*TODO*/ }
+                            ) {
+                                Icon(
+                                    Icons.AutoMirrored.Filled.ArrowBack,
+                                    contentDescription = "Back"
+                                )
+                            }
+                        }
+                    )
+                }
+            ) { padding ->
+                Column(
+                    modifier = Modifier
+                        .padding(padding)
+                        .verticalScroll(rememberScrollState()),
+                ) {
+                    AsyncImage(
+                        model = movie.poster,
+                        contentDescription = "",
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .aspectRatio(16f / 9f),
+                    )
+                    Text(
+                        text = movie.title,
+                        modifier = Modifier.padding(16.dp),
+                        style = MaterialTheme.typography.headlineMedium,
+                    )
+                }
+            }
+        }
+    }
+}
