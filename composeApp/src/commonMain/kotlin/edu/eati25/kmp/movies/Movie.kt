cad6c9c6a6fe2d2efc06a87f4e99f97d0f54fe29
elagarrigue <elagarriguelazarte@doximity.com>
2025-02-18 09:30:15 -0300

    Dummy home

---
+++ composeApp/src/commonMain/kotlin/edu/eati25/kmp/movies/Movie.kt
@@ -0,0 +1,15 @@
+package edu.eati25.kmp.movies
+
+data class Movie(
+    val id: Int,
+    val title: String,
+    val poster: String,
+)
+
+val movies = (0..100).map {
+    Movie(
+        id = it,
+        title = "Movie $it",
+        poster = "https://picsum.photos/200/300?id=$it",
+    )
+}
