6cd6d4e823e7019136cc06ff4ffa8ec0a76a8586
elagarrigue <elagarriguelazarte@doximity.com>
2025-02-22 16:03:03 -0300

    Improve detail

--- composeApp/src/commonMain/kotlin/edu/eati25/kmp/movies/data/MoviesRepository.kt
+++ composeApp/src/commonMain/kotlin/edu/eati25/kmp/movies/data/MoviesRepository.kt
@@ -15,6 +15,17 @@ class MoviesRepository(
     }
 
     private fun RemoteMovie.toDomainMovie(): Movie {
-        return Movie(id, title, "https://image.tmdb.org/t/p/w185$posterPath")
+        return Movie(
+            id = id,
+            title = title,
+            overview = overview,
+            releaseDate = releaseDate,
+            poster = "https://image.tmdb.org/t/p/w185$posterPath",
+            backdrop = backdropPath.let { "https://image.tmdb.org/t/p/w780$it" },
+            originalTitle = originalTitle,
+            originalLanguage = originalLanguage,
+            popularity = popularity,
+            voteAverage = voteAverage
+        )
     }
 }
