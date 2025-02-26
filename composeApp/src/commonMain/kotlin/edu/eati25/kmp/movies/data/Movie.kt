312b617b7ad3ea39cc95620d7333aee3acf690da
elagarrigue <elagarriguelazarte@doximity.com>
2025-02-26 16:24:21 -0300

    Implement db

--- composeApp/src/commonMain/kotlin/edu/eati25/kmp/movies/data/Movie.kt
+++ composeApp/src/commonMain/kotlin/edu/eati25/kmp/movies/data/Movie.kt
@@ -1,6 +1,11 @@
 package edu.eati25.kmp.movies.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
 data class Movie(
+    @PrimaryKey(autoGenerate = true)
     val id: Int,
     val title: String,
     val overview: String,
